c1e4a390302f8733cf3b6bbb99d9d87fd816d0ff
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-29 13:08:29 -0800

    Adds contact form

--- client/src/components/Main/Contact.js
+++ client/src/components/Main/Contact.js
@@ -1,16 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { validateEmail } from '../../utils/helpers';
+import emailjs from 'emailjs-com';
 import './main.css';
 
 function Contact() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-  document.title = 'Contact - Salli Ala Nabi';
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+
+  function handleChange(event) {
+    if (event.target.name === 'email') {
+      const isValid = validateEmail(event.target.value);
+      // console.log(isValid);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!event.target.value.length) {
+        setErrorMessage(`${event.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [event.target.name]: event.target.value })
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(formState);
+    emailjs.sendForm('service_xbl8m76', 'template_qz8l7lm', event.target, 'user_3QlmspmFC6ohkE8C94IEp')
+      .then((result) => {
+      }, (error) => {
+        console.log(error.text);
+      })
+      .then(() => {
+        setErrorMessage('Message sent successfully!');
+        setTimeout(function () { setErrorMessage(''); }, 3000);
+      })
+      .then(() => {
+        event.target.querySelector("input[name='name'").value = "";
+        event.target.querySelector("input[name='email'").value = "";
+        event.target.querySelector("textarea[name='message'").value = "";
+      });
+  }
 
   return (
-    <main className='contact'>
-      <section>
-        <h1>Contact</h1>
+    <>
+      <section key="hero-contact" id="hero-contact" className="hero-images">
+        <img
+          src={ require(`../../assets/images/cairo-skyline.jpg`).default }
+          alt="View of Cairo skyline from Ahmad's home in Giza, Egypt."
+        // width="1920px"
+        // height="900px"
+        />
       </section>
-    </main>
+
+      <main>
+        <section id="contacts" className="container">
+          <h1>Contact Us</h1>
+          <form id="contact-form" onSubmit={ handleSubmit }>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" placeholder="Please type your full name here" defaultValue={ name } onBlur={ handleChange } />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" autoComplete="email" className="validate" placeholder="Please type your email address here" defaultValue={ email } onBlur={ handleChange } />
+            </div>
+            <div>
+              <label htmlFor="message">Message:</label>
+              <textarea id="message-input" type="text" name="message" rows="7" placeholder="Please type your message here" defaultValue={ message } onBlur={ handleChange } />
+            </div>
+            <button id="contact-submit" type="submit" value="send">Submit</button>
+            { errorMessage && (
+              <div>
+                <p id="contact-form-error-message">{ errorMessage }</p>
+              </div>
+            ) }
+          </form>
+        </section>
+      </main>
+    </>
   )
 }
 
--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -11,7 +11,7 @@ function Home() {
         <pre className="sulli">&#xFDFA;</pre>
         <h1 className="">Home</h1>
         <h2 className="">Welcome to Salli Ala Nabi</h2>
-        <p>Salli Ala Nabi includes a Ramadan Tracker that helps you set targets and track your daily activities and acts of worship, in Ramadan, in order to do more good deeds, God willing.</p>
+        <p>Currently, <strong>Salli Ala Nabi</strong> is a Ramadan Tracker that helps you set targets and track your daily activities and acts of worship, in Ramadan, in order to do more good deeds, God willing.</p>
       </section>
     </main>
   )
