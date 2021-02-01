6154ed884942b8ee1b8613d195f2cd6dbc398258
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-02-01 15:29:19 -0800

    Adjusts table column widths in profile component

--- client/src/components/Main/Profile/index.js
+++ client/src/components/Main/Profile/index.js
@@ -26,26 +26,28 @@ function Profile() {
         <h2>Optional Details:</h2>
 
         <table className='mt-20'>
-          <tr>
-            <th>First Name:</th>
-            <td>Ahmad</td>
-            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
-          </tr>
-          <tr>
-            <th>Last Name:</th>
-            <td>El Gamal</td>
-            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
-          </tr>
-          <tr>
-            <th>Gender:</th>
-            <td>Male</td>
-            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
-          </tr>
-          <tr>
-            <th>Date of Birth:</th>
-            <td>08 July 1978</td>
-            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>First Name:</th>
+              <td>Ahmad</td>
+              <td className="d-orange-d4 edit-btn"><button className='blue-grey-l5-text'>Edit</button></td>
+            </tr>
+            <tr>
+              <th>Last Name:</th>
+              <td>El Gamal</td>
+              <td className="d-orange-d4 edit-btn"><button className='blue-grey-l5-text'>Edit</button></td>
+            </tr>
+            <tr>
+              <th>Gender:</th>
+              <td>Male</td>
+              <td className="d-orange-d4 edit-btn"><button className='blue-grey-l5-text'>Edit</button></td>
+            </tr>
+            <tr>
+              <th>Date of Birth:</th>
+              <td>08 July 1978</td>
+              <td className="d-orange-d4 edit-btn"><button className='blue-grey-l5-text'>Edit</button></td>
+            </tr>
+          </tbody>
         </table>
       </section>
     </main>
