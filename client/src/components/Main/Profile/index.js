717cc1699a3fadbb24136e1736653ac71be1359e
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-02-01 15:00:24 -0800

    Creates draft 1 of structure and styling of Profile component

--- client/src/components/Main/Profile/index.js
+++ client/src/components/Main/Profile/index.js
@@ -15,7 +15,38 @@ function Profile() {
 
       <section className="container">
         <h1>Profile</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt odit et itaque eum ducimus sunt voluptates facere quia nulla? Consectetur ipsam perspiciatis error cupiditate laudantium corrupti omnis sint reiciendis fuga!</p>
+        <h2>Required Information:</h2>
+        <h3 className="mt-20">Email: <span className="black-text">ahmadelgamal@gmail.com</span></h3>
+        <div className="flex">
+          <button className="btn d-orange-d4 mt-20">Change Email</button>
+          <button className="btn l-green-d4 mt-20">Change Password</button>
+          <button className="btn d-orange-d4 mt-20">Delete Account</button>
+        </div>
+
+        <h2>Optional Details:</h2>
+
+        <table className='mt-20'>
+          <tr>
+            <th>First Name:</th>
+            <td>Ahmad</td>
+            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
+          </tr>
+          <tr>
+            <th>Last Name:</th>
+            <td>El Gamal</td>
+            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
+          </tr>
+          <tr>
+            <th>Gender:</th>
+            <td>Male</td>
+            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
+          </tr>
+          <tr>
+            <th>Date of Birth:</th>
+            <td>08 July 1978</td>
+            <td className="d-orange-d4"><button className='blue-grey-l5-text'>Edit</button></td>
+          </tr>
+        </table>
       </section>
     </main>
   )
