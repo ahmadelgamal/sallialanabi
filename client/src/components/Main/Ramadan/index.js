d35c0a53214d0c93ea19724e8172cd1d60f7aca6
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-02-03 08:52:17 -0800

    Completes items in Ramadan component

--- client/src/components/Main/Ramadan/index.js
+++ client/src/components/Main/Ramadan/index.js
@@ -187,6 +187,240 @@ function Ramadan() {
               </div>
             </div>
           </article>
+
+          <article>
+            <h3><i>Sunnah ( Nawafil )</i> Prayers</h3>
+            <div className='flex-row'>
+              <div>
+                <label htmlFor="maghrib">2 after Maghrib</label>
+                <br />
+                <input type="checkbox" id="maghrib" name="maghrib" value="maghrib" />
+              </div>
+
+              <div>
+                <label htmlFor="ishaa">2 after Ishaa</label>
+                <br />
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">2 before Fajr</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">1st 2 before Zhuhr</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">2nd 2 before Zhuhr</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">2 after Zhuhr</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3><i>Dhuha</i> Prayers</h3>
+            <div className='flex-row'>
+              <div>
+                <label htmlFor="maghrib">1st 2 Rak'a</label>
+                <br />
+                <input type="checkbox" id="maghrib" name="maghrib" value="maghrib" />
+              </div>
+
+              <div>
+                <label htmlFor="ishaa">2nd 2 Rak'a</label>
+                <br />
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">3rd 2 Rak'a</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">4th 2 Rak'a</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+
+              <div>
+                <label htmlFor="fajr">More:___</label>
+                <br />
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3>Morning/Evening Remembrances</h3>
+            <div className='flex-row'>
+              <div>
+                <label htmlFor="maghrib">Click here for morning remembrance</label>
+                <br />
+                <input type="checkbox" id="maghrib" name="maghrib" value="maghrib" />
+              </div>
+
+              <div>
+                <label htmlFor="ishaa">Click here for evening remembrance</label>
+                <br />
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3>Quran Memorization (3-5 verses)</h3>
+            <div className='flex-row'>
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa"><i>Juz' #:</i></label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="fajr" name="fajr" value="fajr" />
+                <br />
+                <label htmlFor="fajr">From <i>Surah</i>:___ Verse:___ to Surah:___ Verse:___</label>
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3>Study Islamic Sciences</h3>
+            <div className='flex-row'>
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Quran</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Hadith</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Theology</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Jurisprudence</label>
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3>Remembrance</h3>
+            <div className='flex-row'>
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">سبحان الله، والحمد لله، ولا إله إلا الله، والله أكبر، ولا حول ولا قوة إلا بالله</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">أستغفر الله وأتوب إليه</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">سبحان الله وبحمده سبحان الله العظيم</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">حَسْبُنَا اللَّهُ وَنِعْمَ الْوَكِيلُ</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">الصلاة على النبي (بأي صيغة)</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">ـــ</span></p>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Count</label>
+                <p>Thikr: <span className="rtl">ـــ</span></p>
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h3>Charity</h3>
+            <div className='flex-row'>
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Medicine:___</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Food:___</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Clothing:___</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Shelter:___</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Cash:___</label>
+              </div>
+
+              <div>
+                <input type="checkbox" id="ishaa" name="ishaa" value="ishaa" />
+                <br />
+                <label htmlFor="ishaa">Other:___</label>
+              </div>
+            </div>
+          </article>
         </div>
       </section>
     </main>
